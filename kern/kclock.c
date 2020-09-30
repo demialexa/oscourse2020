9f1d764b0a883a7eaa4ff2cd28939e3df586d7f6
Baskov Evgeniy <j-basevgser@yandex.ru>
2020-09-30 21:15:08 +0300

    Implement kernel clock initiallization

--- kern/kclock.c
+++ kern/kclock.c
@@ -6,13 +6,31 @@
 void
 rtc_init(void) {
   nmi_disable();
+
+  uint8_t val;
+
   // LAB 4: Your code here
+  outb(IO_RTC_CMND, RTC_AREG);
+  val = inb(IO_RTC_DATA);
+
+  outb(IO_RTC_CMND, RTC_AREG);
+  outb(IO_RTC_DATA, SET_NEW_RATE(val, RTC_500MS_RATE));
+
+  outb(IO_RTC_CMND, RTC_BREG);
+  val = inb(IO_RTC_DATA);
+
+  outb(IO_RTC_CMND, RTC_BREG);
+  outb(IO_RTC_DATA, val | RTC_PIE);
+
 }
 
 uint8_t
 rtc_check_status(void) {
   uint8_t status = 0;
   // LAB 4: Your code here
+
+  outb(IO_RTC_CMND, RTC_BREG);
+  status = inb(IO_RTC_DATA);
 
   return status;
 }
