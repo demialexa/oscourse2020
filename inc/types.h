6e0b134c0a4540c181cbb50a2d869f832c1d5aae
Baskov Evgeniy <j-basevgser@yandex.ru>
2020-12-01 13:16:44 +0300

    Fix compilation after reformatting

--- inc/types.h
+++ inc/types.h
@@ -14,14 +14,14 @@ typedef int32_t off_t;
 #define MIN(_a, _b) ({      \
     typeof(_a) __a = (_a);  \
     typeof(_b) __b = (_b);  \
-    __a <= __b ? __a : __b; \})
+    __a <= __b ? __a : __b; })
 #endif
 
 #ifndef MAX
 #define MAX(_a, _b) ({      \
     typeof(_a) __a = (_a);  \
     typeof(_b) __b = (_b);  \
-    __a >= __b ? __a : __b; \})
+    __a >= __b ? __a : __b; })
 #endif
 
 /* Rounding operations (efficient when n is a power of 2) */
@@ -29,11 +29,11 @@ typedef int32_t off_t;
 /* Round down to the nearest multiple of n */
 #define ROUNDDOWN(a, n) ({        \
     uint64_t __a = (uint64_t)(a); \
-    (typeof(a))(__a - __a % (n)); \})
+    (typeof(a))(__a - __a % (n)); })
 
 /* Round up to the nearest multiple of n */
 #define ROUNDUP(a, n) ({                                  \
     uint64_t __n = (uint64_t)(n);                         \
-    (typeof(a))(ROUNDDOWN((uint64_t)(a) + __n - 1, __n)); \})
+    (typeof(a))(ROUNDDOWN((uint64_t)(a) + __n - 1, __n)); })
 
 #endif /* !JOS_INC_TYPES_H */
